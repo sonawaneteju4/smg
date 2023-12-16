58ec1bd287e10c9a45bc089b71ce34f482acc12b
Tejas Sonawane <105315165+sonawaneteju4@users.noreply.github.com>
2023-12-16 13:26:14 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Navbar from './Pages/Navbar';
 
 function App() {
   return (
-   <div className='text-center mt-20'> 
+    <>
+    <Navbar/>
+   <div className='text-center'> 
     Sree Ram Medical <br /> Billing And Mangement
    </div>
+    </>
   );
 }
 
---
+++ src/Pages/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='h-10 flex item-center bg-slate-300'>
+        <div>Home</div>
+    </div>
+  )
+}
+
+export default Navbar
