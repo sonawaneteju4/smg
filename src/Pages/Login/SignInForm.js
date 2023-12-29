dcee19405ee6245f9c771505bdf92efa013e7495
Tejas Sonawane <105315165+sonawaneteju4@users.noreply.github.com>
2023-12-29 12:23:48 +0000

    Updates

--- src/Pages/Login/SignInForm.js
+++ src/Pages/Login/SignInForm.js
@@ -2,22 +2,19 @@ import React, { useState } from 'react';
 
 const SignInForm = () => {
   const [userCredential, setuserCredential] = useState({ email: "", password: "" })
-  const [errMessage, seterrMessage] = useState("Contact Admin ")
+  const [errMessage, seterrMessage] = useState("")
 
   const onChange = (e) => {
     setuserCredential({ ...userCredential, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = () => {
-    alert("submit")
+    seterrMessage("Contact Admin Login Is Not Activated")
   }
   return (
     <div className="flex justify-center items-center h-screen">
       <form className="w-full max-w-md bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
-{/*         
-      <small className="text-red-500 mt-2 font-extrabold  text-center">
-            errMessage
-          </small> */}
+
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
             Email
@@ -48,7 +45,11 @@ const SignInForm = () => {
             required
           />
         </div>
-        
+        <div className='mt-2 mb-2'>
+          <small className="text-red-500 mt-2 font-extrabold  text-center">
+            {{ errMessage }}
+          </small>
+        </div>
         <div className="flex items-center justify-between">
           <button
             className=" hover:bg-black bg-black/80 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
