4a770c8f7ca14ac1e472d825c70018833fd36452
Tejas Sonawane <105315165+sonawaneteju4@users.noreply.github.com>
2023-12-29 12:43:56 +0000

    updates

--- src/Pages/Login/LoginPage.js
+++ src/Pages/Login/LoginPage.js
@@ -3,11 +3,11 @@ import SignInForm from './SignInForm'
 
 const LoginPage = () => {
     return (
-        <div className='flex flex-row h-screen '>
+        <div className='lg:flex lg:flex-row sm:flex-col h-screen '>
 
-            <div className='basis-1/2 bg-orange-500 flex flex-col items-center justify-center'>
-                <div className='font-serif font-extrabold text-white text-4xl'>Brand Name</div>
-                <div>Brand Info</div>
+            <div className='basis-1/2  bg-orange-500 flex flex-col items-center justify-center'>
+                <div className='font-serif font-extrabold text-white text-4xl'>Shriram Medical & Surgicals</div>
+                <div className='font-mono'>Sakri</div>
             </div>
             <div className='basis-1/2'>
                 <SignInForm/>
--- src/Pages/Login/SignInForm.js
+++ src/Pages/Login/SignInForm.js
@@ -2,24 +2,25 @@ import React, { useState } from 'react';
 
 const SignInForm = () => {
   const [userCredential, setuserCredential] = useState({ email: "", password: "" })
+  const [errorMsg, setErrorMsg] = useState(false)
 
   const onChange = (e) => {
     setuserCredential({ ...userCredential, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = () => {
-    alert("hello")
+    setErrorMsg(true)
   }
   return (
     <div className="flex justify-center items-center h-screen">
-      <form className="w-full max-w-md bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+      <form className="w-full max-w-md bg-gray-200 shadow-xl rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
 
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
             Email
           </label>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border-2 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="email"
             name="email"
             type="email"
@@ -34,7 +35,7 @@ const SignInForm = () => {
             Password
           </label>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border-2 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
             id="password"
             name='password'
             type="password"
@@ -46,7 +47,7 @@ const SignInForm = () => {
         </div>
         <div className='mt-2 mb-2'>
           <small className="text-red-500 mt-2 font-extrabold  text-center">
-            {/* {{ errMessage }} */}
+            {errorMsg ? (<span>Contact Admin If Any Error While LogIn</span>):null}
           </small>
         </div>
         <div className="flex items-center justify-between">
