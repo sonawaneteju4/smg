66950488f1762c04e300c33759ac595296e36285
Tejas Sonawane <105315165+sonawaneteju4@users.noreply.github.com>
2023-12-31 13:42:03 +0000

    .

--- src/Pages/Login/LoginPage.js
+++ src/Pages/Login/LoginPage.js
@@ -2,17 +2,20 @@ import React from 'react'
 import SignInForm from './SignInForm'
 
 const LoginPage = () => {
+    
     return (
         <div className='lg:flex lg:flex-row sm:flex-col h-screen '>
 
             <div className='basis-1/2  bg-orange-500 flex flex-col items-center justify-center'>
                 <div style={{'font-family': 'Kumar One'}} className=' font-extrabold text-white lg:text-4xl sm:text-xl'>Shriram Medical & Surgicals</div>
-                <div className='font-mono text-2xl text-white'>Sakri</div>
+                <div style={{ 'font-family': 'Kumar One' }} className='font-extrabold text-2xl text-white'>Sakri</div>
             </div>
             <div className='basis-1/2'>
                 <SignInForm/>
             </div>
         </div>
+        
+      
     )
 }
 
--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
     <div className='h-10 bg-orange-500'>
       <div className='flex  justify-between item-center px-5'>
         <div className=' text-white' style={{ 'font-family': 'Kumar One' }}>Shriram Medical & Surgicals  </div>
-        <div className='text-black' style={{ 'font-family': 'mono' }}>SignOut</div>
+        <div className='text-black' style={{ 'font-family': 'Kumar' }}>SignOut</div>
       </div>
     </div>
   ):null}
