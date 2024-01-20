016e448e3576e49745f7f5f8127f2e22539e8508
sonawaneteju4 <sonawaneteju4@gmail.com>
2024-01-21 00:46:46 +0530

    <Analytics />

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import LoginPage from './Pages/Login/LoginPage';
-import Navbar from './Pages/Navbar';
-import Dashboard from './Pages/Dashboard';
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from './firebaseConfig';
-import { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import LoginPage from "./Pages/Login/LoginPage";
+import Navbar from "./Pages/Navbar";
+import Dashboard from "./Pages/Dashboard";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebaseConfig";
+import { useEffect, useState } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-  const [islogin, setislogin] = useState(true)
+  const [islogin, setislogin] = useState(true);
   useEffect(() => {
-    onAuthStateChanged(
-      auth,
-      (user) => {
-        if (!user) {
-          setislogin(false);
-          console.log("userIsLogin" + islogin)
-        } else {
-          setislogin(true);
-
-        }
-      },
-    );
-  }, [islogin])
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        setislogin(false);
+        console.log("userIsLogin" + islogin);
+      } else {
+        setislogin(true);
+      }
+    });
+  }, [islogin]);
   return (
     <>
+      <Analytics />
       <BrowserRouter>
         {islogin && <Navbar />}
         <Routes>
