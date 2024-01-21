5402d4c9a8451ffa757844e22d510443b316c478
sonawaneteju4 <sonawaneteju4@gmail.com>
2024-01-21 14:48:43 +0530

    ErrorAlert-Update

--- src/Pages/CreditBills/QuickBill.js
+++ src/Pages/CreditBills/QuickBill.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const QuickBill = ({onClick}) => {
+const QuickBill = ({ onClick }) => {
   return (
     <>
       <div className="fixed z-10 inset-0 overflow-y-auto">
@@ -18,6 +18,49 @@ const QuickBill = ({onClick}) => {
                 </button>
               </div>
             </div>
+            <form className="flex mx-5 my-2">
+              <div className="mx-2 w-20">
+                <select
+                  name="cType"
+                  // value={customerData.cType}
+                  // onChange={handleChange}
+                  id="customer_name"
+                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  required
+                >
+                  <option className="" value="pageNo" selected>
+                    Page No
+                  </option>
+                  <option className="" value="phoneNO">
+                    Phone No
+                  </option>
+                </select>
+                <label
+                  for="customer_name"
+                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  Customer Type
+                </label>
+              </div>
+              <div className="w-full">
+                <input
+                  type="text"
+                  name="searchInput"
+                  id="searchInput"
+                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder="Search By Phone Or Page Number"
+                  // onChange={handleChange}
+                  required
+                />
+                <label
+                  for="searchInput"
+                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  Page Number
+                </label>
+              </div>
+            </form>
+            
           </div>
         </div>
       </div>
